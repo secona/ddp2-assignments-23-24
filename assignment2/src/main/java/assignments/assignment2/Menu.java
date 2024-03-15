45e37e267db1570a66dc24539b1fdfbfbe88572c
secona <77039267+secona@users.noreply.github.com>
2024-03-15 16:03:15 +0700

    feat: comments in Menu

--- assignment2/src/main/java/assignments/assignment2/Menu.java
+++ assignment2/src/main/java/assignments/assignment2/Menu.java
@@ -4,19 +4,30 @@ public class Menu {
 	private String namaMakanan;
 	private double harga;
 
-	public String getNamaMakanan() {
-		return namaMakanan;
-	}
-
-	public double getHarga() {
-		return harga;
-	}
-
 	public Menu(String namaMakanan, double harga) {
 		this.namaMakanan = namaMakanan;
 		this.harga = harga;
 	}
 
+	/**
+	 * Getter untuk instance variable namaMakanan
+	 * @return nama makanan
+	 */
+	public String getNamaMakanan() {
+		return namaMakanan;
+	}
+
+	/**
+	 * Getter untuk instance variable harga
+	 * @return harga
+	 */
+	public double getHarga() {
+		return harga;
+	}
+
+	/**
+	 * @return "{namaMakanan} {harga}"
+	 */
 	@Override
 	public String toString() {
 		return String.format("%s %d", this.namaMakanan, (int) this.harga);
